e4aafac67b5e3e8bfb99cf5797ee96d44f3511a1
JeongJin <38030774+TypingOK@users.noreply.github.com>
2021-03-16 20:48:23 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+//import App from "./App";
+
+const title = Response.potentiallyMaliciousInput;
+const element = <h1>{title}</h1>;
+ReactDOM.render = (element, document.getElementById("root"));
